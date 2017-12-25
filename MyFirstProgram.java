de5fd9267d7e1ce82d9abc1af77ed9c423085e20
dnimic <dnimic@gmail.com>
2017-12-25 14:40:06 +0300

    My first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,11 @@
+/**
+ * My very first program
+ */
+class MyFirstProgram {
+
+    public static void main(String args[]){
+
+        System.out.println("\n\tJava drives me crazy!\n");
+
+    }		
+}
